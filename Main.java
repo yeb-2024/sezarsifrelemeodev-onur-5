7ebc9b8902b90acb85da9e49679e4fc417ad26af
HP <seyfeddinonur055@gmail.com>
2025-02-19 15:47:31 +0300

    buton giriş ve sonuç kısmı yapıldı

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class    Main extends JFrame implements ActionListener {
+    JButton şifreleme, şifreçözme;
+    JTextField anahter, mesaj;
+    JTextArea sonuç;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
